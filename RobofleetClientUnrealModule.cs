f344be2552e1559c43a9d7ce7b88e52773a336e6
zotoli <zotoli@hotmail.com>
2021-04-06 13:51:37 -0500

    Initial module structure

---
+++ RobofleetClientUnrealModule.cs
@@ -0,0 +1,34 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+using System.IO;
+using UnrealBuildTool;
+
+public class RobofleetClientUnrealModule : ModuleRules
+{
+    // convenience properties
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
+
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
+
+    public RobofleetClientUnrealModule(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] {"WebSockets" });
+
+        //LoadViconLibs(Target);
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+    }
+}
