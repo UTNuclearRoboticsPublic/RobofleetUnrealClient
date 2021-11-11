ef8d940609f59f30aab20546049aba2e4a7a5274
frank-Regal <fregal@utexas.edu>
2021-11-11 11:37:03 -0600

    Added all robotfleet structs in message_structs

--- Public/RobofleetBPMessageStructs.h
+++ Public/RobofleetBPMessageStructs.h
@@ -6,15 +6,38 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "RobofleetBPMessageStructs.generated.h"
 
-/**
- *
+/*
+ * ROS message clones to message_structs.h in robofleet_client_lib
  */
+
+USTRUCT(BlueprintType)
+struct FTime
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	int32 _sec;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	int32 _nsec;
+};
+
+
+
 USTRUCT(BlueprintType)
 struct FHeader
 {
 	GENERATED_BODY()
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
-	float x;
+	int32 seq;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	FTime stamp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	FString frame_id;
+
 
 };
 
@@ -22,9 +45,18 @@ USTRUCT(BlueprintType)
 struct FRobotStatus
 {
 	GENERATED_BODY()
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
-	float x;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	FString status;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	bool is_ok;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	float battery_level;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	FString location;
 };
 
 USTRUCT(BlueprintType)
@@ -33,5 +65,17 @@ struct FRobotLocationStamped
 	GENERATED_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	FHeader header;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
 	float x;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	float y;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	float z;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Robofleet")
+	float theta;
 };
